8080d705e4edecb622109d418e4cdb8c909cc829
João Henrique <joaoenrique.san@gmail.com>
2022-07-17 17:48:55 -0300

    feat: Add and stylize the form

--- js/index.js
+++ js/index.js
@@ -1,2 +1,33 @@
 const backgroundComponent = document.querySelector("#mainContainer");
 backgroundComponent.style.backgroundImage = SVG_BACKGROUNDS.default;
+
+const textInput = document.querySelector("#cityInput");
+const searchButton = document.querySelector("#searchButton");
+const loadingAnimation = searchButton.querySelector("#loadingAnimation");
+const buttonText = searchButton.querySelector("p");
+
+const getCityInputWeatherData = async () => {
+  const cityName = textInput.value;
+  if (cityName.trim() === "") {
+    alert("The city name cannot be empty");
+    return;
+  }
+  loadCityWeather(cityName);
+};
+
+const loadCityWeather = async (cityName) => {
+  buttonText.style.display = "none";
+  loadingAnimation.style.display = "block";
+
+  try {
+    const currCityWeatherData = await getWeatherData(cityName);
+    console.log(currCityWeatherData);
+  } catch (err) {
+    alert("There was an error getting the data");
+  }
+
+  buttonText.style.display = "block";
+  loadingAnimation.style.display = "none";
+};
+
+searchButton.addEventListener("click", getCityInputWeatherData);
